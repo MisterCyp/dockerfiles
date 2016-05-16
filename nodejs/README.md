642992ed80f2c9b000861a8c36642125c921254b
MisterCyp <mistercyp59@gmail.com>
2016-05-16 18:06:02 +0200

    Menage dans les dockerfiles

--- nodejs/README.md
+++
@@ -1,4 +0,0 @@
-## wonderfall/node
-**SIZE : ±38MB**
-
-![](https://i.goopics.net/lq.png)
--- reverse/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-touch /var/run/nginx.pid
-chown -R $UID:$GID /etc/nginx /var/log/nginx /var/run/nginx.pid /sites-enabled /conf.d /certs /var/www
-chmod -R 700 /certs
-
-/usr/bin/supervisord -c /etc/supervisord.conf
